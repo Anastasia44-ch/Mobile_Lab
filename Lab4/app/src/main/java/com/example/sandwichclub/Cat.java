bb55b56f99d4ef4848bce3358327964b45eb996c
Anastasia44 <chertkova.ad@mail.ru>
2020-11-20 03:04:41 +0700

    4)Создаем класс Cat и выполняем конвертацию экземпляра этого класса в JSON, а затем обратно с другими свойствами объекта. 5)Выдаем результат в текстовое поле TextView. 6)Добавляем компонент View и закрашиваем его цветом кота.

---
+++ Lab4/app/src/main/java/com/example/sandwichclub/Cat.java
@@ -0,0 +1,15 @@
+package com.example.sandwichclub;
+
+public class Cat {
+    public String name;     // Имя
+    public int age;     // Возраст
+    public int color;       // Цвет
+
+    // Конструктор
+    public Cat(){
+
+    }
+
+
+}
+
--- Lab4/app/src/main/java/com/example/sandwichclub/MainActivity.java
+++ Lab4/app/src/main/java/com/example/sandwichclub/MainActivity.java
@@ -1,16 +1,52 @@
 package com.example.sandwichclub;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public
-class MainActivity extends AppCompatActivity {
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    TextView tv_from;
+    TextView tv_color;
+
 
     @Override
-    protected
-    void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.id_tv);
+        tv_from = findViewById(R.id.id_tv_from);
+        tv_color = findViewById(R.id.id_tv_color);
+
+        Cat murzik = new Cat();
+        murzik.name = "Мурзик";
+        murzik.age = 4;
+        murzik.color = Color.RED;
+
+        // Конвертация экземпляра класс Cat в JSON
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        Log.i("GSON", gson.toJson(murzik));
+        String text_gson = gson.toJson(murzik).toString();
+        tv.setText(text_gson);
+
+        // Конвертация JSON в объект
+        String jsonText = "{'name':'Мурзик','color':-16777216,'age':8}";
+        Cat barsik = gson.fromJson(jsonText, Cat.class);
+        Log.i("GSON","Имя: " + barsik.name + "\nВозраст: " + barsik.age);
+        tv_from.setText("Кот: " + barsik.name + ", " + barsik.age + " лет, " + " цвет: ");
+        // Добавил компонент Textview и закрасил его цветом кота.
+        tv_color.setBackgroundColor(barsik.color);
+
     }
 }
