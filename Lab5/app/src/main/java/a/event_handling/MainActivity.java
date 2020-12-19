fb3777b795f10d1b00668f1ee7d95f09243f2a7e
Anastasia44 <chertkova.ad@mail.ru>
2020-12-19 19:23:54 +0700

    4.Определите функционал меню таким образом, чтобы при выборе:

--- Lab5/app/src/main/java/a/event_handling/MainActivity.java
+++ Lab5/app/src/main/java/a/event_handling/MainActivity.java
@@ -7,34 +7,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentManager;
-
 import java.util.Objects;
-
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().setReorderingAllowed(true)
                     .add(R.id.fragment_container_view, ButtonClick.class, null).commit();
         }
     }
-
     public  boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
     }
-
     public void menu_activity(MenuItem item){
         FragmentManager fgmg = getSupportFragmentManager();
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         Objects.requireNonNull(getSupportActionBar()).setSubtitle(item.getTitle());
-
         switch (item.getItemId()){
             case R.id.menuitem_bc:
                 fgmg.beginTransaction().replace(R.id.fragment_container_view, ButtonClick.class, null)
@@ -43,16 +35,12 @@ public class MainActivity extends AppCompatActivity {
                 return;
 
             case R.id.menuitem_me:
-                fgmg.beginTransaction().replace(R.id.fragment_container_view, MotionEvent.class, null)
+                fgmg.beginTransaction().replace(R.id.fragment_container_view, MotionEvent_act.class, null)
                         .setReorderingAllowed(true).addToBackStack("name").commit();
                 return;
-
             case R.id.menuitem_cg:
                 fgmg.beginTransaction().replace(R.id.fragment_container_view, CommonGestures.class, null)
                         .setReorderingAllowed(true).addToBackStack("name").commit();
-
         }
     }
-
-
 }
