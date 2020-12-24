d907ae5b18b31f9821216cf99abcdf28429461f6
Anastasia44 <chertkova.ad@mail.ru>
2020-12-24 17:21:35 +0700

    6.Добавьте в код PhotoGallery объект RecyclerView для списка фотографий и создайте класс PhotoAdapter для реализации RecyclerView. В качестве LayoutManager для RecyclerView установите GridLayoutManager (таблица с 3 столбцами).

---
+++ Lab6.new/app/src/main/java/com/example/photogallery/PhotoAdapter.java
@@ -0,0 +1,53 @@
+package com.example.photogallery;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.photogallery.model.Photo;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+
+public class PhotoAdapter extends RecyclerView.Adapter<PhotoAdapter.ViewHolder> {
+    private final List<Photo> values;
+
+
+    public PhotoAdapter(List<Photo> items) {
+        values = items;
+    }
+
+    @Override
+    @NonNull
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        Photo photo = values.get(position);
+        Picasso.get().load(photo.getUrlS()).into(holder.image);
+        holder.itemView.setTag(photo);
+    }
+
+    @Override
+    public int getItemCount() {
+        return values.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        final ImageView image;
+
+
+    }
+
+}
--- Lab6.new/app/src/main/java/com/example/photogallery/PhotoGallery.java
+++ Lab6.new/app/src/main/java/com/example/photogallery/PhotoGallery.java
@@ -1,16 +1,23 @@
 package com.example.photogallery;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public
-class PhotoGallery extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhotoGallery extends AppCompatActivity {
 
     @Override
     protected
     void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gallery_activity);
+
+        final RecyclerView recycler = findViewById(R.id.rView);
+        recycler.setLayoutManager(new GridLayoutManager(this, 3));
     }
 }
